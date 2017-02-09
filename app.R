7a0b8b61a7ce3b5ca0492ffb7e536b4777325fad
Grigory Okhotnikov <grishk@ro.ru>
2017-02-09 12:22:37 +0300

    Rearrange layout

--- app.R
+++ app.R
@@ -4,29 +4,29 @@ library(shinythemes)
 # By default, the file size limit is 5MB. It can be changed by
 # setting this option. Here we'll raise limit to 1024MB.
 options(shiny.maxRequestSize = 1024*1024^2)
+df <- NULL
+col.names <- c()
 
 server <- function(input, output) {
-    output$contents <- renderTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
+  getTable <- reactive({
+    df <<- read.csv(input$datafile$datapath, header = input$header,
+             sep = input$sep, quote = input$quote)
+    col.names <<- colnames(df)
+    df
+  })
+  
+  output$maintable <- DT::renderDataTable({
     inFile <- input$datafile
-    
+    isolate(inFile)
     if (is.null(inFile))
       return(NULL)
-    
-    read.csv(inFile$datapath, header = input$header,
-             sep = input$sep, quote = input$quote)
-    })
-
-    output$mytable1 <- DT::renderDataTable({
-    inFile <- input$datafile
-    DT::datatable(read.csv(inFile$datapath, header = input$header,
-    sep = input$sep, quote = input$quote))
-})
+    DT::datatable(getTable(),
+                  options=list(pageLength=10, 
+                               lengthMenu=list(c(5, 10, 30, 100, -1), c('5', '10', '30', '100', 'Все')),
+                               searching=FALSE,
+                               language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Russian.json')
+                  ))
+  })
   
 }
 
@@ -34,9 +34,9 @@ ui = tagList(
   navbarPage(
     theme = shinytheme("spacelab"),
     "Clustering",
-    tabPanel("Navbar 1",
+    tabPanel("Данные",
              sidebarPanel(
-               fileInput('datafile', 'Choose file to upload',
+               fileInput('datafile', 'Выбрать файл для загрузки',
                          accept = c(
                            'text/csv',
                            'text/comma-separated-values',
@@ -46,57 +46,42 @@ ui = tagList(
                            '.tsv'
                          )
                ),
-               tags$hr(),
-               checkboxInput('header', 'Header', TRUE),
-               radioButtons('sep', 'Separator',
-                            c(Comma=',',
-                              Semicolon=';',
-                              Tab='\t'),
+               checkboxInput('header', 'Заголовок', TRUE),
+               radioButtons('sep', 'Разделитель:',
+                            c('Запятая'=',',
+                              'Точка с запятой'=';',
+                              'Табуляция'='\t'),
                             ','),
-               radioButtons('quote', 'Quote',
-                            c(None='',
-                              'Double Quote'='"',
-                              'Single Quote'="'"),
-                            '"'),
+               #               radioButtons('quote', 'Кавычки',
+               #                            c('Отсутствуют'='',
+               #                              'Двойные кавычки'='"',
+               #                              'Одинарные кавычки'="'"),
+               #                            '"'),
                tags$hr(),
-               p('If you want a sample .csv or .tsv file to upload,',
-                 'you can first download the sample',
-                 a(href = 'mtcars.csv', 'mtcars.csv'), 'or',
-                 a(href = 'pressure.tsv', 'pressure.tsv'),
-                 'files, and then try uploading them.'
+               selectizeInput(
+                 'sortColumns', 'Сортировать по', choices = colnames(df), multiple = TRUE
                ),
                tags$hr(),
-               textInput("txt", "Text input:", "general"),
-               sliderInput("slider", "Slider input:", 1, 100, 30),
-               tags$h5("Deafult actionButton:"),
-               actionButton("action", "Search"),
-
-               tags$h5("actionButton with CSS class:"),
-               actionButton("action2", "Action button", class = "btn-primary")
-
+               p('Для тестирования можете загрузить примеры файлов .csv or .tsv:',
+                 a(href = 'mtcars.csv', 'mtcars.csv'), 'или',
+                 a(href = 'pressure.tsv', 'pressure.tsv')
+               )
              ),
              mainPanel(
-               tabsetPanel(
-                 tabPanel("Tab 1",
-                          h4("Table"),
-                          tableOutput("contents"),
-                          
-                          h4("This sample is doing nothing"),
-                          verbatimTextOutput("text output"),
-                          h1("Header 1"),
-                          h2("Header 2"),
-                          h3("Header 3"),
-                          h4("Header 4"),
-                          h5("Header 5")
-                 ),
-		tabPanel("Tab 2",
-			DT::dataTableOutput('mytable1')),
-		tabPanel("Tab 3")
-               )
-             )
+               DT::dataTableOutput('maintable'))
     ),
-    tabPanel("Navbar 2"),
-    tabPanel("Navbar 3")
+    tabPanel("Информация",
+             sidebarPanel(),
+             mainPanel()
+    ),
+    tabPanel("Корреляция",
+             sidebarPanel(),
+             mainPanel()
+    ),
+    tabPanel("Кластеризация",
+             sidebarPanel(),
+             mainPanel()
+    )
   )
 )
 
